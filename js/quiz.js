a0ff6ebfb3918b389ca21413760bbae52cbdff27
Chris Underwood <cunderwoodmn@gmail.com>
2016-09-26 15:49:07 -0700

    added 3 questions and answers in 2 dimensional array

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,10 @@
+var questions = [
+  ["How many states are in the United States?", 50],
+  ['How many continents are there?', 7],
+  ['How many legs does an insect have?', 6]
+];
+
+
 
 function print(message) {
   document.write(message);

7f0300b41678cd613bd63503f350b54fa44c9aaf
Chris Underwood <cunderwoodmn@gmail.com>
2016-09-26 15:56:41 -0700

    created additional variables and started the quiz loop

--- js/quiz.js
+++ js/quiz.js
@@ -4,8 +4,18 @@ var questions = [
   ['How many legs does an insect have?', 6]
 ];
 
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var html;
 
 
 function print(message) {
   document.write(message);
+}
+
+for (var z = 0; z < questions.length; z += 1) {
+  question = questions[i][0];
+  answer = questions[i][1];
 }
